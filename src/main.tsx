fbd0ca4fc992f385dd6f0a3c761064d92a6d9b05
m4rcin7 <mrku1337@gmail.com>
2025-05-14 19:05:47 +0200

    implement react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/HomePage";
+import Price from "./pages/Price";
+import Resources from "./pages/Resources";
+import Solutions from "./pages/Solutions";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
+  {
+    path: "/price",
+    element: <Price />,
+  },
+  {
+    path: "/resources",
+    element: <Resources />,
+  },
+  {
+    path: "/solutions",
+    element: <Solutions />,
+  },
 ]);
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
